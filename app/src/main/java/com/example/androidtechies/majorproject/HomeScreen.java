692159c396ddaf598a7665853897c3a7dbe863b4
Sheshank-Srivastava <ambitious9350164795@gmail.com>
2018-03-30 14:15:18 +0530

    Added the newest update from mantor

--- app/src/main/java/com/example/androidtechies/majorproject/HomeScreen.java
+++ app/src/main/java/com/example/androidtechies/majorproject/HomeScreen.java
@@ -6,25 +6,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-<<<<<<< HEAD
-public class HomeScreen extends AppCompatActivity {
-    public static final String HomeScreenTag = "HomeScreen";
-    public static final Integer cseValue = 0;
-    public static final Integer itValue = 1;
-    public static final Integer eceValue = 2;
-    public static final Integer eeeValue = 3;
-=======
 import com.example.androidtechies.majorproject.RoomSample.AppDatabase;
 import com.example.androidtechies.majorproject.RoomSample.DatabaseInitializer;
 
 public class HomeScreen extends AppCompatActivity {
     public static final String HomeScreenTag = "HomeScreen";
     public static final Integer cseValue = 0;
-    public static final Integer it = 0;
-    public static final Integer ece = 0;
-    public static final Integer eee = 0;
+    public static final Integer itValue = 1;
+    public static final Integer eceValue = 2;
+    public static final Integer eeeValue = 3;
     AppDatabase appDatabase;
->>>>>>> 410389ff97e9d50f3246ef333c98e2167f3c0e4a
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +29,7 @@ public class HomeScreen extends AppCompatActivity {
         cse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-<<<<<<< HEAD
+
                 Intent cseIntent = new Intent(HomeScreen.this, ListPage.class);
                 cseIntent.putExtra(HomeScreenTag, cseValue);
                 startActivity(cseIntent);
@@ -50,64 +42,39 @@ public class HomeScreen extends AppCompatActivity {
 //                Intent eceIntent = new Intent(HomeScreen.this, ListPage.class);
 //                eceIntent.putExtra(HomeScreenTag, eceValue);
 //                startActivity(eceIntent);
-=======
-//                Intent cseIntent = new Intent(HomeScreen.this, ListPage.class);
-//                cseIntent.putExtra(HomeScreenTag, cseValue);
-//                startActivity(cseIntent);
-                populateDatabase();
 
-            }
-        });
-        
-
-        
-        
-//        ece.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent ece = new Intent(HomeScreen.this, ECEHomeScreen.class);
-//                startActivity(ece);
->>>>>>> 410389ff97e9d50f3246ef333c98e2167f3c0e4a
 //
 //            }
 //        });
 //        it.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
-<<<<<<< HEAD
+
 //                Intent itIntent = new Intent(HomeScreen.this, ListPage.class);
 //
 //                itIntent.putExtra(HomeScreenTag, itValue);
 //                startActivity(itIntent);
-=======
-//                Intent it = new Intent(HomeScreen.this, ITHomeScreen.class);
-//                startActivity(it);
->>>>>>> 410389ff97e9d50f3246ef333c98e2167f3c0e4a
+
 //
 //            }
 //        });
 //        eee.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
-<<<<<<< HEAD
+
 //                Intent eeeIntent = new Intent(HomeScreen.this, ListPage.class);
 //                eeeIntent.putExtra(HomeScreenTag,eeeValue);
 //                startActivity(eeeIntent);
-=======
-//                Intent eee = new Intent(HomeScreen.this, EEEHomeScreen.class);
-//                startActivity(eee);
->>>>>>> 410389ff97e9d50f3246ef333c98e2167f3c0e4a
+
 //
 //            }
 //        });
     }
-<<<<<<< HEAD
-    }
-=======
+
 
     private void populateDatabase() {
         DatabaseInitializer.populateAsync(AppDatabase.getAppDatabase(this));
     }
 }
->>>>>>> 410389ff97e9d50f3246ef333c98e2167f3c0e4a
+
 
